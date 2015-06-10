ec759db96ae8a3e899e97a2ad3cdb9e4c57b8cb5
unknown <t_tappa@ORE4NKBN32.ads.autodesk.com>
2015-06-10 14:02:31 -0700

    Fixing

---
+++ main.js
@@ -0,0 +1,46 @@
+var app = angular.module("todoApp", []);
+
+app.directive('item', function(){
+    return {
+        restrict: 'E',
+        template: '<div class="text-center">' +
+        '<h3><li>{{items.toString()}}</li></h3>' +
+        '</div>'
+    }
+});
+
+app.directive('header', function(){
+    return {
+        restrict: 'E',
+        template: '<div class="jumbotron text-center">' +
+        '<h1>To-Do List</h1>' +
+        '</div>' +
+        '<br>'
+    }
+});
+
+app.directive('inputbox', function(){
+    return {
+        restrict: 'E',
+        template: '<div>' +
+        '<input type="text" class="form-control text-center" placeholder="Enter In an Item" ng-model="inputText">' +
+        '</div>' +
+        '<br>'
+    }
+});
+
+app.directive('enterbutton', function(){
+    return {
+        restrict: 'E',
+        template: '<div class="text-center">' +
+        '<button class="btn btn-info btn-block" style="text-align:center" ng-click="items.push({text:inputText}); inputText=\'\'"> ' +
+        '<h3>Add Item</h3>' +
+        '</button>' +
+        '</div>'
+    }
+});
+
+app.controller('itemCtrl', function($scope){
+    $scope.items=[{text:"Alex"}];
+    $scope.inputText = "";
+});
