395bc0cfd4018e80570fb132df4cb462c8c65505
Alexander Tappin <alextappin@gmail.com>
2015-06-11 10:31:27 -0700

    Check Boxes and deletes

--- main.js
+++ main.js
@@ -3,8 +3,37 @@ var app = angular.module("todoApp", []);
 app.directive('item', function(){
     return {
         restrict: 'E',
-        template: '<div class="text-center">' +
-        '<h3><ul><li ng-repeat="item in items">{{item.text}}</li></ul></h3>' +
+        template: '<div ng-repeat="item in items">' +
+            '<div ng-if="!item.done">' +
+            '<h3><ul><li>' +
+            '<div class="text-center" style="padding-right: 15px">'+
+            '<input type=checkbox style="margin-top: 10px;float: left" class="glyphicon glyphicon-unchecked" ng-click="item.done = true">' +
+            '<span style="padding-left: 13px">' +
+            '{{item.text}}' +
+            '</span>' +
+            '<input type=checkbox style="margin-top: 10px;float: right" class="glyphicon glyphicon-remove-sign" ng-click="items.splice($index,1)">' +
+            '</div>' +
+            '</li></ul></h3>' +
+            '</div>' +
+            '</div>'
+    }
+});
+
+app.directive("doneitem", function(){
+    return {
+        restrict: 'E',
+        template: '<div ng-repeat="item in items">' +
+        '<div ng-if="item.done">' +
+        '<h3><ul><li>' +
+        '<div class="text-center" style="padding-right: 15px; text-decoration: line-through";>'+
+        '<input type=checkbox style="margin-top: 10px;float: left" class="glyphicon glyphicon-check" ng-click="item.done = false" ng-checked="true">' +
+        '<span style="color: rgb(10,50,50); opacity: .5; padding-left: 13px">' +
+        '{{item.text}}' +
+        '</span>' +
+        '<input type=checkbox style="margin-top: 10px;float: right" class="glyphicon glyphicon-remove-sign" ng-click="items.splice($index,1)">' +
+        '</div>' +
+        '</li></ul></h3>' +
+        '</div>' +
         '</div>'
     }
 });
@@ -13,19 +42,19 @@ app.directive('header', function(){
     return {
         restrict: 'E',
         template: '<div class="jumbotron text-center">' +
-        '<h1>To-Do List</h1>' +
-        '</div>' +
-        '<br>'
+            '<h1>To-Do List</h1>' +
+            '</div>' +
+            '<br>'
     }
 });
 
 app.directive('inputbox', function(){
     return {
         restrict: 'E',
-        template: '<div>' +
-        '<input type="text" class="form-control text-center" placeholder="Enter In an Item" ng-model="inputText">' +
-        '</div>' +
-        '<br>'
+        template: '<br><div>' +
+            '<input type="text" class="form-control text-center" placeholder="Enter In an Item" ng-model="inputText">' +
+            '</div>' +
+            '<br>'
     }
 });
 
@@ -33,10 +62,10 @@ app.directive('enterbox', function(){
     return {
         restrict: 'E',
         template: '<div class="text-center">' +
-        '<button class="btn btn-info btn-block" style="text-align:center" ng-click="items.push({text:inputText}); inputText=\'\'"> ' +
-        '<h3>Add Item</h3>' +
-        '</button>' +
-        '</div>'
+            '<button class="btn btn-info btn-block" style="text-align:center" ng-click="items.push({text:inputText, done:false}); inputText=\'\';"> ' +
+            '<h3>Add Item</h3>' +
+            '</button>' +
+            '</div>'
     }
 });
 
@@ -47,6 +76,7 @@ app.directive('app', function(){
             '<div class="container">' +
             '<header></header>' +
             '<item></item>' +
+            '<doneitem></doneitem>' +
             '<form>' +
             '<inputbox></inputbox>' +
             '<enterbox></enterbox>' +
@@ -57,6 +87,6 @@ app.directive('app', function(){
 });
 
 app.controller('itemCtrl', function($scope){
-    $scope.items=[{text:"", done:false}];
+    $scope.items=[{text:"Example Item", done:false}];
     $scope.inputText = "";
 });
